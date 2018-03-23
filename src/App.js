65784a230c9e2a9b818c70ac3ba16f9d87bee1bb
Rikk Hill <rikk@unashamedly.co.uk>
2018-03-23 21:55:27 +0000

    Corrected JSX error

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h2>Rikk Hill</h2>
-      <p>Rikk is amazing and although he's not looking for a job right now, you should totally give him one</p>
+      <div>
+        <h2>Rikk Hill</h2>
+        <p>Rikk is amazing and although he's not looking for a job right now, you should totally give him one</p>
+      </div>
     );
   }
 }
